9a04b64d71b2668b73b9b00ba655cee0314448d9
Yann-Cast <yann.castagnetti@epfedu.fr>
2016-03-29 23:59:35 +0200

    Fix perte de données chanegment de page et iteration des calculs stat

--- js/mainController.js
+++ js/mainController.js
@@ -2,12 +2,15 @@
 
 // Fichier contenant le controller principal qui englobe le Body. Il gere le chargement du Json
 var dataJson;
+var dataJsonBis;
+var uninitializedChart=true;
+var chartData={};
 var meteoriteControllers=angular.module('meteoriteControllers', []);
 
 meteoriteControllers.controller('mainController',['$http','$scope','$location',function($http,$scope,$location){
-	
+
 	//Fonction de redirection vers la page "map"
-	var currentLocation=$location;	
+	var currentLocation=$location;
 	function changePath(){
 		currentLocation.path('/map');
 	}
@@ -16,17 +19,15 @@ meteoriteControllers.controller('mainController',['$http','$scope','$location',f
 		.success(function(data){
 			console.log("success import json");
 			dataJson=data;
+			dataJsonBis = JSON.parse(JSON.stringify(dataJson));
+
 			//Une fois que le Json est chargé, on peut rediriger vers la map
 			changePath();
 		})
 		.error(function(errormsg){
 			alert("error chargement json");
 		})
-		
-		
-		
+
+
+
 }]);
-
-
-
-
